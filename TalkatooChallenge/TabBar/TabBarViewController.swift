ddf8ecf828c61a21a9df684d51e8f78db4a0fc99
Matthew Musoke <msk.matt2@gmail.com>
2021-08-20 11:11:57 -0300

    Inital commit plus some initial building out of the UI

---
+++ TalkatooChallenge/TabBar/TabBarViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarViewController.swift
+//  TalkatooChallenge
+//
+//  Created by mskmatt on 19/08/2021.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+}
